8d6b3452084706a9165a9670f1b103f3e39b8e3e
Rolando-Barbella <rolandobarbella@gmail.com>
2014-04-22 17:26:58 +0100

    Doing some training with Fizz Buzz

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,40 +1,57 @@
 describe("Fizzbuzz", function(){
 
-  it("should tell number if is divisible by 3", function(){
-    expect(isDivideByThree(3)).toBe(true);
+ it("Should tell when the number divisible by 3", function(){
+		expect(isDivideByThree(3)).toBe(true)
   });
 
-  it("should tell number if is divisible by 5", function(){
-    expect(isDivideByFive(5)).toBe(true);
-
+ it("Should tell when the number is divisble by 5", function(){
+	  expect(isDivideByFive(5)).toBe(true)
   });
 
-  it("should tell number if is divisible by 3 and 5", function(){
-    expect(isDivideByFifteen(15)).toBe(true);
-  });
-  it ("should return number if is not divisible by 3 or 4", function(){
-    expect(isDivideByFifteen(4)).toBe(false);
-  });
+ it("Should tell when a number is divisible by 15",function(){
+   expect(isDivideByFifteen(15)).toBe(true)
+ });
 
 });
 
-describe("Play", function() {
 
-  it ("should tell fizz if the number is divisible by 3",function(){
-    expect(Fizz(15)).toEqual("Fizzbuzz")
-  });
+// describe("Fizzbuzz", function(){
 
-  it ("should tell fizz if the number is divisible by 3",function(){
-    expect(Fizz(3)).toEqual("Fizz")
-  });
+//   it("should tell number if is divisible by 3", function(){
+//     expect(isDivideByThree(3)).toBe(true);
+//   });
 
-  it ("should tell Buzz if the number is divisible by 5",function(){
-    expect(Fizz(5)).toEqual("Buzz")
-  });
+//   it("should tell number if is divisible by 5", function(){
+//     expect(isDivideByFive(5)).toBe(true);
+
+//   });
+
+//   it("should tell number if is divisible by 3 and 5", function(){
+//     expect(isDivideByFifteen(15)).toBe(true);
+//   });
+//   it ("should return number if is not divisible by 3 or 4", function(){
+//     expect(isDivideByFifteen(4)).toBe(false);
+//   });
+
+// });
+
+// describe("Play", function() {
+
+//   it ("should tell fizz if the number is divisible by 3",function(){
+//     expect(Fizz(15)).toEqual("Fizzbuzz")
+//   });
+
+//   it ("should tell fizz if the number is divisible by 3",function(){
+//     expect(Fizz(3)).toEqual("Fizz")
+//   });
+
+//   it ("should tell Buzz if the number is divisible by 5",function(){
+//     expect(Fizz(5)).toEqual("Buzz")
+//   });
 
 
-  it ("should return number if is not divisible by 3 and 5",function(){
-    expect(Fizz(4)).toEqual(4)
-  });
+//   it ("should return number if is not divisible by 3 and 5",function(){
+//     expect(Fizz(4)).toEqual(4)
+//   });
 
-});
+// });
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,25 +1,44 @@
-function FizzBuzz() {
+function FizzBuzz(){
 }
 
-function MakeDivision(divisor){
-	return function(number){
-		return number % divisor == 0;
-	}
-};
-
-	isDivideByThree = MakeDivision(3)
-	isDivideByFive = MakeDivision(5)
-	isDivideByFifteen = MakeDivision(15)
-
-	Fizz = function(number){
-	 if (isDivideByFifteen(number)== true){
-	 	return"Fizzbuzz";
-	 }
-	 else if(isDivideByFive(number)== true){
-		return "Buzz";
-		}
-		else if(isDivideByThree(number)== true){
-		return "Fizz";
-		}
-		else return number;
+	function isDivideByThree(number){
+	  if(number%3==0){
+	  return true;
+	  }
 	};
+
+	function isDivideByFive(number){
+		if(number%5 == 0){
+			return true;
+		}
+	};
+
+	function isDivideByFifteen(number){
+		if(numbe%15 == 0){
+			return true;
+		}
+	};
+
+
+// function MakeDivision(divisor){
+// 	return function(number){
+// 		return number % divisor == 0;
+// 	}
+// };
+
+// 	isDivideByThree = MakeDivision(3)
+// 	isDivideByFive = MakeDivision(5)
+// 	isDivideByFifteen = MakeDivision(15)
+
+// 	Fizz = function(number){
+// 	 if (isDivideByFifteen(number)== true){
+// 	 	return"Fizzbuzz";
+// 	 }
+// 	 else if(isDivideByFive(number)== true){
+// 		return "Buzz";
+// 		}
+// 		else if(isDivideByThree(number)== true){
+// 		return "Fizz";
+// 		}
+// 		else return number;
+// 	};
