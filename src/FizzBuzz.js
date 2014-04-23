06f5d7e843d150d6ee832eed798dd7b2204024bc
Rolando Barbella <rolandobarbella@gmail.com>
2014-04-23 16:12:02 +0100

    Update FizzBuzz.js

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,41 +1,41 @@
 function FizzBuzz(){
 }
 
-	function isDivideByThree(number){
-	  if(number%3==0){
-	  return true;
-	  }
-	};
+function isDivideByThree(number){
+  if(number%3==0){
+  return true;
+  }
+};
 
-	function isDivideByFive(number){
-		if(number%5==0){
-			return true;
-		}
-	};
+function isDivideByFive(number){
+  if(number%5==0){
+  return true;
+   }
+};
 
-	function isDivideByFifteen(number){
-		if(number%15 == 0){
-			return true;
-		}
-	};
+function isDivideByFifteen(number){
+   if(number%15 == 0){
+   return true;
+    }
+};
 
 function MakeDivision(number,divider){
-	if(number % divider == 0){
-	return true;
-	}
-	else{
-	return false;
-	}
+   if(number % divider == 0){
+    return true;
+    }
+    else{
+    return false;
+    }
 };
 
 function MakeDivision2(){
-	var number = 15;
-	var divider = 5;
-	return number%divider == 0;
+  var number = 15;
+  var divider = 5;
+  return number%divider == 0;
 }
 
 function MakeNumberDivisor(number){
-	return function(divisor){
+   return function(divisor){
    return number%divisor == 0;
 	}
 };
@@ -44,21 +44,21 @@ function MakeNumberDivisor(number){
    isDivideByFive = MakeNumberDivisor(5);
    isDivideByFifteen = MakeNumberDivisor(15);
 
-   Play = function(number){
-   	if(isDivideByThree(number) == true){
-   		return"Fizz";
-   	}
-   	else if(isDivideByFive(number)== true){
-   		return"Buzz";
-   	}
+    Play = function(number){
+	   if(isDivideByThree(number) == true){
+	   	return"Fizz";
+	   }
+	   else if(isDivideByFive(number)== true){
+	   	return"Buzz";
+	   }
+	
+	   else if(isDivideByFifteen(number)== true){
+	   	return"Fizz Buzz";
+	   }
+	
+	else{
+	    return number
+	 }
 
-   	else if(isDivideByFifteen(number)== true){
-   		return"Fizz Buzz";
-   	}
-
-    else{
-    	return number
-    }
-
-   };
+     };
 
